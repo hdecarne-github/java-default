d1c6068a311e5dd388e4d747792905e02a8f1f04
Holger <hdecarne@gmail.com>
2016-11-14 21:40:44 +0100

    More testing code added.

---
+++ src/test/java/de/carne/test/StringsTest.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright (c) 2016 Holger de Carne and contributors, All Rights Reserved.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package de.carne.test;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import de.carne.util.Strings;
+
+/**
+ * Test {@link Strings} class functionality.
+ */
+public class StringsTest {
+
+	/**
+	 * Test {@link Strings} class functionality.
+	 */
+	@Test
+	public void test() {
+		Assert.assertTrue(Strings.isEmpty(null));
+		Assert.assertTrue(Strings.isEmpty(""));
+		Assert.assertFalse(Strings.isEmpty(" "));
+		Assert.assertFalse(Strings.notEmpty(null));
+		Assert.assertFalse(Strings.notEmpty(""));
+		Assert.assertTrue(Strings.notEmpty(" "));
+		Assert.assertEquals("", Strings.safe(null));
+		Assert.assertEquals("", Strings.safe(""));
+		Assert.assertEquals(" ", Strings.safe(" "));
+		Assert.assertEquals(null, Strings.safeTrim(null));
+		Assert.assertEquals("", Strings.safeTrim(""));
+		Assert.assertEquals("", Strings.safeTrim(" "));
+	}
+
+}
