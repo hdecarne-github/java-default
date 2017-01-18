f5745465ab73e279e49e59808e9e9477d8bf3939
Holger <hdecarne@gmail.com>
2017-01-18 21:35:47 +0100

    More testing code added.

--- src/test/java/de/carne/test/util/prefs/PrefsTest.java
+++ src/test/java/de/carne/test/util/prefs/PrefsTest.java
@@ -33,7 +33,12 @@ import org.junit.Test;
 import de.carne.check.NonNullByDefault;
 import de.carne.io.IOHelper;
 import de.carne.util.Exceptions;
+import de.carne.util.prefs.BooleanPreference;
+import de.carne.util.prefs.IntPreference;
+import de.carne.util.prefs.LongPreference;
+import de.carne.util.prefs.PathPreference;
 import de.carne.util.prefs.PropertiesPreferencesFactory;
+import de.carne.util.prefs.StringPreference;
 
 /**
  * Test {@link PropertiesPreferencesFactory} class.
@@ -93,6 +98,7 @@ public class PrefsTest {
 		readPreferences1();
 		removePreferences();
 		readPreferences2();
+		clearPreferences();
 	}
 
 	private void dumpPropertiesPath() throws IOException {
@@ -153,6 +159,131 @@ public class PrefsTest {
 		Assert.assertArrayEquals(new String[] {}, rootKeys);
 		Arrays.sort(rootChildrenNames);
 		Assert.assertArrayEquals(new String[] {}, rootChildrenNames);
+	}
+
+	private void clearPreferences() throws BackingStoreException {
+		Preferences root = PropertiesPreferencesFactory.customRoot(PROPERTIES_PATH);
+
+		root.clear();
+	}
+
+	/**
+	 * Test {@link StringPreference} class.
+	 */
+	@Test
+	public void testStringPreference() {
+		Preferences root = PropertiesPreferencesFactory.customRoot(PROPERTIES_PATH);
+		StringPreference preference = new StringPreference(root, "stringPreference");
+
+		Assert.assertEquals(null, preference.get());
+
+		preference.put(getClass().getSimpleName());
+
+		Assert.assertEquals(getClass().getSimpleName(), preference.get());
+	}
+
+	/**
+	 * Test {@link BooleanPreference} class.
+	 */
+	@Test
+	public void testBooleanPreference() {
+		Preferences root = PropertiesPreferencesFactory.customRoot(PROPERTIES_PATH);
+		BooleanPreference preference = new BooleanPreference(root, "booleanPreference");
+
+		Assert.assertEquals(null, preference.get());
+		Assert.assertEquals(true, preference.getBoolean(true));
+
+		preference.putBoolean(true);
+
+		Assert.assertEquals(Boolean.TRUE, preference.get());
+		Assert.assertEquals(true, preference.getBoolean(false));
+
+		preference.put(Boolean.FALSE);
+
+		Assert.assertEquals(Boolean.FALSE, preference.get());
+		Assert.assertEquals(false, preference.getBoolean(true));
+
+		root.put(preference.key(), "xyz");
+
+		Assert.assertEquals(Boolean.FALSE, preference.get());
+		Assert.assertEquals(true, preference.getBoolean(true));
+	}
+
+	/**
+	 * Test {@link IntPreference} class.
+	 */
+	@Test
+	public void testIntPreference() {
+		Preferences root = PropertiesPreferencesFactory.customRoot(PROPERTIES_PATH);
+		IntPreference preference = new IntPreference(root, "intPreference");
+
+		Assert.assertEquals(null, preference.get());
+		Assert.assertEquals(1, preference.getInt(1));
+
+		preference.putInt(1);
+
+		Assert.assertEquals(Integer.valueOf(1), preference.get());
+		Assert.assertEquals(1, preference.getInt(0));
+
+		preference.put(Integer.valueOf(-1));
+
+		Assert.assertEquals(Integer.valueOf(-1), preference.get());
+		Assert.assertEquals(-1, preference.getInt(0));
+
+		root.put(preference.key(), "xyz");
+
+		Assert.assertEquals(null, preference.get());
+		Assert.assertEquals(1, preference.getInt(1));
+	}
+
+	/**
+	 * Test {@link LongPreference} class.
+	 */
+	@Test
+	public void testLongPreference() {
+		Preferences root = PropertiesPreferencesFactory.customRoot(PROPERTIES_PATH);
+		LongPreference preference = new LongPreference(root, "longPreference");
+
+		Assert.assertEquals(null, preference.get());
+		Assert.assertEquals(1, preference.getLong(1));
+
+		preference.putLong(1);
+
+		Assert.assertEquals(Long.valueOf(1), preference.get());
+		Assert.assertEquals(1, preference.getLong(0));
+
+		preference.put(Long.valueOf(-1));
+
+		Assert.assertEquals(Long.valueOf(-1), preference.get());
+		Assert.assertEquals(-1, preference.getLong(0));
+
+		root.put(preference.key(), "xyz");
+
+		Assert.assertEquals(null, preference.get());
+		Assert.assertEquals(1, preference.getLong(1));
+	}
+
+	/**
+	 * Test {@link PathPreference} class.
+	 */
+	@Test
+	public void testDirectoryPreference() {
+		Preferences root = PropertiesPreferencesFactory.customRoot(PROPERTIES_PATH);
+		PathPreference preference = new PathPreference(root, "directoryPreference", PathPreference.IS_REGULAR_FILE);
+
+		Assert.assertEquals(null, preference.get());
+
+		preference.put(PROPERTIES_PATH);
+
+		Assert.assertEquals(PROPERTIES_PATH, preference.get());
+
+		preference.put(PROPERTIES_PATH.getParent().resolve("unknown.dat"));
+
+		Assert.assertEquals(null, preference.get());
+
+		root.put(preference.key(), "xyz");
+
+		Assert.assertEquals(null, preference.get());
 	}
 
 }
