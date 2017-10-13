ad6a553a5429452f046be1cc145f1f12a0ed7087
Holger <hdecarne@gmail.com>
2017-10-13 23:41:10 +0200

    Add FileAttribute utilities

---
+++ src/main/java/de/carne/nio/file/attribute/FileAttributes.java
@@ -0,0 +1,69 @@
+/*
+ * Copyright (c) 2016-2017 Holger de Carne and contributors, All Rights Reserved.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package de.carne.nio.file.attribute;
+
+import java.nio.file.FileSystem;
+import java.nio.file.Path;
+import java.nio.file.attribute.FileAttribute;
+import java.nio.file.attribute.PosixFilePermission;
+import java.nio.file.attribute.PosixFilePermissions;
+import java.util.ArrayList;
+import java.util.EnumSet;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * Utility class providing {@linkplain FileAttribute} related functions.
+ */
+public final class FileAttributes {
+
+	private FileAttributes() {
+		// Prevent instantiation
+	}
+
+	private static final String POSIX = "posix";
+
+	/**
+	 * Get the {@linkplain FileSystem} specific best choice for the access rights for a user's private directory.
+	 *
+	 * @param fileSystem The {@linkplain FileSystem} to determine the access rights for.
+	 * @return The access rights to use for a user's private directory.
+	 */
+	@SuppressWarnings("squid:S1452")
+	public static FileAttribute<?>[] userDirectoryDefault(FileSystem fileSystem) {
+		List<FileAttribute<?>> userDefault = new ArrayList<>();
+		Set<String> fileAttributeViews = fileSystem.supportedFileAttributeViews();
+
+		if (fileAttributeViews.contains(POSIX)) {
+			userDefault.add(PosixFilePermissions.asFileAttribute(EnumSet.of(PosixFilePermission.OWNER_READ,
+					PosixFilePermission.OWNER_WRITE, PosixFilePermission.OWNER_EXECUTE)));
+		}
+		return userDefault.toArray(new FileAttribute<?>[userDefault.size()]);
+	}
+
+	/**
+	 * Get the {@linkplain FileSystem} specific best choice for the access rights for a user's private directory.
+	 *
+	 * @param path The {@linkplain Path} to determine the access rights for.
+	 * @return The access rights to use for a user's private directory.
+	 */
+	@SuppressWarnings("squid:S1452")
+	public static FileAttribute<?>[] userDirectoryDefault(Path path) {
+		return userDirectoryDefault(path.getFileSystem());
+	}
+
+}
