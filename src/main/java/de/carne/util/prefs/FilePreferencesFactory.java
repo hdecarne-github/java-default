3c44d3cbfd1b5349e6d9e9770e78fe0c52166fce
Holger <hdecarne@gmail.com>
2017-10-13 23:40:46 +0200

    Add config file based preferences support

---
+++ src/main/java/de/carne/util/prefs/FilePreferencesFactory.java
@@ -0,0 +1,118 @@
+/*
+ * Copyright (c) 2016-2017 Holger de Carne and contributors, All Rights Reserved.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package de.carne.util.prefs;
+
+import java.io.File;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Properties;
+import java.util.prefs.Preferences;
+import java.util.prefs.PreferencesFactory;
+
+import de.carne.check.Nullable;
+import de.carne.util.Exceptions;
+
+/**
+ * Custom {@linkplain PreferencesFactory} implementation for storing preferences into human readable text (*.conf)
+ * files.
+ */
+public class FilePreferencesFactory implements PreferencesFactory {
+
+	@Nullable
+	private static final String STORE_HOME = System.getProperty(FilePreferences.class.getName());
+
+	@Override
+	public Preferences systemRoot() {
+		Path storeFile = resolveStoreHomeFile("user.conf");
+
+		return FilePreferencesStore.fromFile(storeFile).root();
+	}
+
+	@Override
+	public Preferences userRoot() {
+		String systemName = getSystemName();
+		Path storeFile = resolveStoreHomeFile("system." + systemName + ".conf");
+
+		return FilePreferencesStore.fromFile(storeFile).root();
+	}
+
+	/**
+	 * Create a {@linkplain Preferences} object backed up by a custom configuration file.
+	 * <p>
+	 * The created {@linkplain Preferences} object can be stored if the submitted custom configuration file is writable
+	 * by the VM.
+	 *
+	 * @param file The custom configuration file to use as a preference store.
+	 * @return The created {@linkplain Preferences} object.
+	 */
+	public static Preferences customRoot(Path file) {
+		return FilePreferencesStore.fromFile(file).root();
+	}
+
+	/**
+	 * Create a {@linkplain Preferences} object backed up by a custom configuration file.
+	 * <p>
+	 * The created {@linkplain Preferences} object can be stored if the submitted custom configuration file is writable
+	 * by the VM.
+	 *
+	 * @param file The custom configuration file to use as a preference store.
+	 * @return The created {@linkplain Preferences} object.
+	 */
+	public static Preferences customRoot(File file) {
+		return customRoot(file.toPath());
+	}
+
+	/**
+	 * Create a {@linkplain Preferences} object backed up by a {@linkplain Properties} object.
+	 * <p>
+	 * The created {@linkplain Preferences} object cannot be stored.
+	 *
+	 * @param data The {@linkplain Properties} object defining the preference data.
+	 * @return The created {@linkplain Preferences} object.
+	 */
+	public static Preferences customRoot(Properties data) {
+		return FilePreferencesStore.fromData(data).root();
+	}
+
+	private static Path resolveStoreHomeFile(String name) {
+		Path userHome = Paths.get(System.getProperty("user.home", "."));
+
+		if (STORE_HOME == null) {
+			throw new IllegalStateException(
+					"No store home defined in system property: " + FilePreferences.class.getName());
+		}
+
+		Path storeHome = userHome.resolve(STORE_HOME);
+
+		return storeHome.resolve(name);
+	}
+
+	private static String getSystemName() {
+		String systemName;
+
+		try {
+			systemName = InetAddress.getLocalHost().getHostName();
+		} catch (UnknownHostException e) {
+			Exceptions.ignore(e);
+			systemName = "localhost";
+		}
+		return systemName;
+	}
+
+}
---
+++ src/test/java/de/carne/test/util/prefs/FilePreferencesTest.java
@@ -0,0 +1,177 @@
+/*
+ * Copyright (c) 2016-2017 Holger de Carne and contributors, All Rights Reserved.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package de.carne.test.util.prefs;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+import java.util.prefs.BackingStoreException;
+import java.util.prefs.Preferences;
+
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.ClassRule;
+import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+
+import de.carne.util.prefs.FilePreferencesFactory;
+
+/**
+ * Test {@linkplain FilePreferencesFactory} class.
+ */
+public class FilePreferencesTest {
+
+	/**
+	 * Temporary HOME folder for config file creation.
+	 */
+	@ClassRule
+	public static final TemporaryFolder HOME = new TemporaryFolder();
+
+	/**
+	 * Setup the necessary system properties.
+	 */
+	@BeforeClass
+	public static void setUpBeforeClass() {
+		System.setProperty("java.util.prefs.PreferencesFactory", FilePreferencesFactory.class.getName());
+		System.setProperty("de.carne.util.prefs.FilePreferences", HOME.getRoot().getAbsolutePath());
+	}
+
+	/**
+	 * Test custom {@linkplain Preferences} access.
+	 *
+	 * @throws IOException if an I/ error occurs.
+	 * @throws BackingStoreException if a backing store error occurs.
+	 */
+	@Test
+	public void testCustomPreferences() throws IOException, BackingStoreException {
+		Preferences customPrefs1 = FilePreferencesFactory.customRoot(loadData());
+
+		Assert.assertEquals(1, customPrefs1.keys().length);
+		Assert.assertEquals(2, customPrefs1.childrenNames().length);
+
+		for (String key : customPrefs1.keys()) {
+			customPrefs1.remove(key);
+		}
+		for (String childrenName : customPrefs1.childrenNames()) {
+			customPrefs1.node(childrenName).removeNode();
+		}
+
+		Assert.assertEquals(0, customPrefs1.keys().length);
+		Assert.assertEquals(0, customPrefs1.childrenNames().length);
+
+		Preferences customPrefs2 = FilePreferencesFactory.customRoot(loadData());
+
+		copyPrefs(customPrefs2, customPrefs1);
+		verifyPrefs(customPrefs2, customPrefs1);
+		customPrefs1.sync();
+		verifyPrefs(customPrefs2, customPrefs1);
+		copyPrefs(customPrefs2, customPrefs1);
+		verifyPrefs(customPrefs2, customPrefs1);
+		customPrefs1.flush();
+		verifyPrefs(customPrefs2, customPrefs1);
+	}
+
+	/**
+	 * Test user {@linkplain Preferences} access.
+	 *
+	 * @throws IOException if an I/ error occurs.
+	 * @throws BackingStoreException if a backing store error occurs.
+	 */
+	@Test
+	public void testUserPreferences() throws IOException, BackingStoreException {
+		Preferences referencePrefs = FilePreferencesFactory.customRoot(loadData());
+		Preferences userPrefs1 = Preferences.userRoot();
+
+		Assert.assertEquals(0, userPrefs1.keys().length);
+		Assert.assertEquals(0, userPrefs1.childrenNames().length);
+
+		copyPrefs(referencePrefs, userPrefs1);
+		verifyPrefs(referencePrefs, userPrefs1);
+		userPrefs1.sync();
+		copyPrefs(referencePrefs, userPrefs1);
+		verifyPrefs(referencePrefs, userPrefs1);
+		userPrefs1.flush();
+		verifyPrefs(referencePrefs, userPrefs1);
+
+		Preferences userPrefs2 = Preferences.userRoot();
+
+		verifyPrefs(referencePrefs, userPrefs2);
+	}
+
+	/**
+	 * Test system {@linkplain Preferences} access.
+	 *
+	 * @throws IOException if an I/ error occurs.
+	 * @throws BackingStoreException if a backing store error occurs.
+	 */
+	@Test
+	public void testSystemPreferences() throws IOException, BackingStoreException {
+		Preferences referencePrefs = FilePreferencesFactory.customRoot(loadData());
+		Preferences systemPrefs1 = Preferences.systemRoot();
+
+		Assert.assertEquals(0, systemPrefs1.keys().length);
+		Assert.assertEquals(0, systemPrefs1.childrenNames().length);
+
+		copyPrefs(referencePrefs, systemPrefs1);
+		verifyPrefs(referencePrefs, systemPrefs1);
+		systemPrefs1.sync();
+		copyPrefs(referencePrefs, systemPrefs1);
+		verifyPrefs(referencePrefs, systemPrefs1);
+		systemPrefs1.flush();
+		verifyPrefs(referencePrefs, systemPrefs1);
+
+		Preferences systemPrefs2 = Preferences.userRoot();
+
+		verifyPrefs(referencePrefs, systemPrefs2);
+	}
+
+	private Properties loadData() throws IOException {
+		Properties data = new Properties();
+
+		try (InputStream dataStream = getClass().getResourceAsStream(getClass().getSimpleName() + ".properties")) {
+			data.load(dataStream);
+		}
+		return data;
+	}
+
+	private void copyPrefs(Preferences from, Preferences to) throws BackingStoreException {
+		for (String key : from.keys()) {
+			to.put(key, from.get(key, null));
+		}
+		for (String childrenName : from.childrenNames()) {
+			Preferences subFrom = from.node(childrenName);
+			Preferences subTo = to.node(childrenName);
+
+			copyPrefs(subFrom, subTo);
+		}
+	}
+
+	private void verifyPrefs(Preferences expected, Preferences actual) throws BackingStoreException {
+		Assert.assertEquals(expected.keys().length, actual.keys().length);
+		for (String key : expected.keys()) {
+			Assert.assertEquals(expected.get(key, null), actual.get(key, key));
+		}
+		Assert.assertEquals(expected.childrenNames().length, actual.childrenNames().length);
+		for (String childrenName : expected.childrenNames()) {
+			Preferences subExpected = expected.node(childrenName);
+			Preferences subActual = actual.node(childrenName);
+
+			verifyPrefs(subExpected, subActual);
+		}
+	}
+
+}
