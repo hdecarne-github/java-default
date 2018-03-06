f33c86395b8d21150281ecff4c753064327fcc83
Holger <hdecarne@gmail.com>
2018-03-06 22:51:16 +0100

    Platform Utility class added

---
+++ src/main/java/de/carne/util/Platform.java
@@ -0,0 +1,60 @@
+/*
+ * Copyright (c) 2016-2018 Holger de Carne and contributors, All Rights Reserved.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package de.carne.util;
+
+import de.carne.check.Check;
+
+/**
+ * Utility class providing functions to determine platform type and capabilities.
+ */
+public class Platform {
+
+	private Platform() {
+		// Prevent instantiation
+	}
+
+	/**
+	 * System property: {@code "os.arch"}
+	 */
+	public static final String SYSTEM_OS_ARCH = Check.notNull(System.getProperty("os.arch"));
+
+	/**
+	 * System property: {@code "os.name"}
+	 */
+	public static final String SYSTEM_OS_NAME = Check.notNull(System.getProperty("os.name"));
+
+	/**
+	 * System property: {@code "os.version"}
+	 */
+	public static final String SYSTEM_OS_VERSION = Check.notNull(System.getProperty("os.version"));
+
+	/**
+	 * Operating System: Linux
+	 */
+	public static final boolean IS_LINUX = SYSTEM_OS_NAME.toUpperCase().startsWith("LINUX");
+
+	/**
+	 * Operating System: macOS
+	 */
+	public static final boolean IS_MAC_OS = SYSTEM_OS_NAME.startsWith("Mac OS X");
+
+	/**
+	 * Operating System: WINDOWS
+	 */
+	public static final boolean IS_WINDOWS = SYSTEM_OS_NAME.startsWith("Windows");
+
+}
---
+++ src/test/java/de/carne/test/util/PlatformTest.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright (c) 2016-2018 Holger de Carne and contributors, All Rights Reserved.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package de.carne.test.util;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import de.carne.util.Platform;
+
+/**
+ * Test {@linkplain Platform} class.
+ */
+class PlatformTest {
+
+	@Test
+	void testOSDetection() {
+		System.out.println("OS_ARCH: " + Platform.SYSTEM_OS_ARCH);
+		System.out.println("OS_NAME: " + Platform.SYSTEM_OS_NAME);
+		System.out.println("OS_VERSION: " + Platform.SYSTEM_OS_VERSION);
+
+		boolean[] osFlags = new boolean[] { Platform.IS_LINUX, Platform.IS_MAC_OS, Platform.IS_WINDOWS };
+		int matchIndex = -1;
+
+		for (int osFlagIndex = 0; osFlagIndex < osFlags.length; osFlagIndex++) {
+			if (osFlags[osFlagIndex]) {
+				Assertions.assertTrue(matchIndex < 0,
+						"Duplicate OS match (index1: " + matchIndex + " index2: " + osFlagIndex);
+				matchIndex = osFlagIndex;
+			}
+		}
+		Assertions.assertTrue(matchIndex >= 0, "No OS match for: '" + Platform.SYSTEM_OS_NAME + "'");
+	}
+
+}
