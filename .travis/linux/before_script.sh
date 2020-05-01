999d9d1199b85547bbad548f3f1020ee85c40a2a
Holger <hdecarne@gmail.com>
2020-05-01 10:00:52 +0200

    Update JDK prior to test run

--- .travis/linux/before_script.sh
+++ .travis/linux/before_script.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -x
 
 curl -sL https://github.com/shyiko/jabba/raw/master/install.sh | bash -s -- --skip-rc && . ~/.jabba/jabba.sh
-jabba install adopt@~1.11.0
+jabba install adopt@1.11.0-6
 ./gradlew -v
