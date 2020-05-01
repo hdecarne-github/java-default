f9baf6709c0bcf559a9b9ec3756d8254006cd684
Holger <hdecarne@gmail.com>
2020-05-01 09:39:49 +0200

    Update JDK prior to test run

--- .travis/linux/before_script.sh
+++ .travis/linux/before_script.sh
@@ -1,3 +1,5 @@
 #!/bin/bash -x
 
-install-jdk.sh -F 11 -L GPL
+curl -sL https://github.com/shyiko/jabba/raw/master/install.sh | bash -s -- --skip-rc && . ~/.jabba/jabba.sh
+jabba ls-remote
+jabba install adopt@11
