79398ac5790f1e6aaa3e4faf80b7b7d70c60c45b
Holger <hdecarne@gmail.com>
2020-05-03 21:19:31 +0200

    Re-factor CI scripts

--- .travis/osx/before_install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash -x
-
-curl -sL https://github.com/shyiko/jabba/raw/master/install.sh | bash -s -- --skip-rc && . ~/.jabba/jabba.sh
-jabba ls-remote
-jabba install $BUILD_JDK
-java -Xmx32m -version 
-./gradlew -v
