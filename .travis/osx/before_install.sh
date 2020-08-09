7110df009ffb901b3a856d2273be0773566b1ee4
Holger <hdecarne@gmail.com>
2020-08-09 06:50:15 +0200

    Make CI script more generic

--- .travis/osx/before_install.sh
+++ .travis/osx/before_install.sh
@@ -1,3 +1,4 @@
 #!/bin/bash -x
 
-curl -sL https://github.com/shyiko/jabba/raw/master/install.sh | bash && . ~/.jabba/jabba.sh
+curl -sL https://github.com/shyiko/jabba/raw/master/install.sh | bash
+export JABBA_BEFORE_INSTALL=".. ~/.jabba/jabba.sh; jabba ls-remote"
--- .travis/windows/before_install.sh
+++ .travis/windows/before_install.sh
@@ -1,4 +1,5 @@
 #!/bin/bash -x
 
 export PATH="$HOME/.jabba/bin/:$PATH"
-powershell -Command "[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; Invoke-Expression ( Invoke-WebRequest https://raw.githubusercontent.com/shyiko/jabba/master/install.ps1 -UseBasicParsing ).Content"
+powershell -ExecutionPolicy Bypass -Command "[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; Invoke-Expression ( Invoke-WebRequest https://raw.githubusercontent.com/shyiko/jabba/master/install.ps1 -UseBasicParsing ).Content"
+export JABBA_BEFORE_INSTALL="jabba ls-remote"
