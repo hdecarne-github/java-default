3612b8529e5c206d8e83f0fd02e276017fe0a105
Holger <hdecarne@gmail.com>
2020-08-09 07:04:54 +0200

    Make CI script more generic

--- .travis/windows/before_install.sh
+++ .travis/windows/before_install.sh
@@ -2,5 +2,4 @@
 
 powershell -ExecutionPolicy Bypass -Command "[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; Invoke-Expression ( Invoke-WebRequest https://raw.githubusercontent.com/shyiko/jabba/master/install.ps1 -UseBasicParsing ).Content"
 export PATH="$HOME/.jabba/bin/:$PATH"
-jabba
 jabba ls-remote
