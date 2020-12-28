b4287b899aa08ff86bea775ade9a14aa25d66c3e
Holger <hdecarne@gmail.com>
2020-12-28 22:23:52 +0100

    Add ByteString class for immutable byte string handling

---
+++ src/main/java/de/carne/util/ByteString.java
@@ -0,0 +1,206 @@
+/*
+ * Copyright (c) 2016-2020 Holger de Carne and contributors, All Rights Reserved.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package de.carne.util;
+
+import java.io.Serializable;
+import java.util.Arrays;
+
+import org.eclipse.jdt.annotation.Nullable;
+
+import de.carne.text.HexFormat;
+
+/**
+ * Immutable byte string support.
+ */
+public final class ByteString implements Serializable, Comparable<ByteString> {
+
+	// Serialization support
+	private static final long serialVersionUID = 2167856394985799551L;
+
+	/**
+	 * The empty byte string.
+	 */
+	public static final ByteString EMPTY = new ByteString(new byte[0]);
+
+	private final byte[] bytes;
+	private final int start;
+	private final int length;
+
+	private ByteString(byte[] bytes) {
+		this(bytes, 0, bytes.length);
+	}
+
+	private ByteString(byte[] bytes, int start, int length) {
+		this.bytes = bytes;
+		this.start = start;
+		this.length = length;
+	}
+
+	/**
+	 * Constructs a new {@linkplain ByteString} instance by wrapping the submitted bytes directly.
+	 *
+	 * @param bytes the bytes to wrap.
+	 * @return the constructed {@linkplain ByteString} instance.
+	 */
+	public static ByteString wrap(byte... bytes) {
+		return (bytes.length > 0 ? new ByteString(bytes) : EMPTY);
+	}
+
+	/**
+	 * Constructs a new {@linkplain ByteString} instance by wrapping the submitted bytes directly.
+	 *
+	 * @param bytes the bytes to wrap.
+	 * @param start the index of the first byte to wrap.
+	 * @param length the number of bytes to wrap.
+	 * @return the constructed {@linkplain ByteString} instance.
+	 */
+	public static ByteString wrap(byte[] bytes, int start, int length) {
+		Check.isTrue(0 <= start);
+		Check.isTrue(0 <= length);
+		Check.isTrue(start + length <= bytes.length);
+
+		return (length > 0 ? new ByteString(bytes, start, length) : EMPTY);
+	}
+
+	/**
+	 * Constructs a new {@linkplain ByteString} instance by copying the submitted bytes.
+	 *
+	 * @param bytes the bytes to copy.
+	 * @return the constructed {@linkplain ByteString} instance.
+	 */
+	public static ByteString copy(byte... bytes) {
+		return copy(bytes, 0, bytes.length);
+	}
+
+	/**
+	 * Constructs a new {@linkplain ByteString} instance by copying the submitted bytes.
+	 *
+	 * @param bytes the bytes to copy.
+	 * @param start the index of the first byte to copy.
+	 * @param length the number of bytes to copy.
+	 * @return the constructed {@linkplain ByteString} instance.
+	 */
+	public static ByteString copy(byte[] bytes, int start, int length) {
+		Check.isTrue(0 <= start);
+		Check.isTrue(0 <= length);
+		Check.isTrue(start + length <= bytes.length);
+
+		return (length > 0 ? new ByteString(Arrays.copyOfRange(bytes, start, start + length)) : EMPTY);
+	}
+
+	/**
+	 * Gets the length of this instance.
+	 *
+	 * @return the length of this instance.
+	 */
+	public int length() {
+		return this.length;
+	}
+
+	/**
+	 * Gets the this instance's byte value at the given index.
+	 *
+	 * @param index the index of the byte to get.
+	 * @return the byte value at the given index
+	 */
+	public byte byteAt(int index) {
+		Check.isTrue(0 <= index);
+		Check.isTrue(index < this.length);
+
+		return this.bytes[this.start + index];
+	}
+
+	/**
+	 * Copies this instances bytes to a destination buffer.
+	 *
+	 * @param dest the buffer to copy into.
+	 * @param destPos the buffer position to start copying at.
+	 */
+	public void copyTo(byte[] dest, int destPos) {
+		Check.isTrue(0 <= destPos);
+		Check.isTrue(dest.length - destPos >= this.length);
+
+		System.arraycopy(this.bytes, this.start, dest, destPos, this.length);
+	}
+
+	/**
+	 * Slices a sub-section from this instance.
+	 *
+	 * @param sliceStart the index of the first byte to slice.
+	 * @param sliceLength the number of bytes to slice.
+	 * @return the sliced byte string.
+	 */
+	public ByteString slice(int sliceStart, int sliceLength) {
+		Check.isTrue(this.start <= sliceStart);
+		Check.isTrue(sliceStart + sliceLength <= this.length);
+
+		return new ByteString(this.bytes, this.start + sliceStart, sliceLength);
+	}
+
+	@Override
+	public int compareTo(ByteString o) {
+		int byteIndex1 = this.start;
+		int byteIndexLimit1 = this.start + this.length;
+		int byteIndex2 = o.start;
+		int byteIndexLImit2 = o.start + o.length;
+		int comparison = 0;
+
+		while (byteIndex1 < byteIndexLimit1 && byteIndex2 < byteIndexLImit2 && comparison == 0) {
+			comparison = Integer.compare(Byte.toUnsignedInt(this.bytes[byteIndex1]),
+					Byte.toUnsignedInt(o.bytes[byteIndex2]));
+			byteIndex1++;
+			byteIndex2++;
+		}
+		if (comparison == 0) {
+			if (byteIndex1 < byteIndexLimit1) {
+				comparison = 1;
+			} else if (byteIndex2 < byteIndexLImit2) {
+				comparison = -1;
+			}
+		}
+		return comparison;
+	}
+
+	@Override
+	public int hashCode() {
+		int hashLimit = Math.min(4, this.length);
+		int hash = 0;
+
+		for (int hashIndex = 0; hashIndex < hashLimit; hashIndex++) {
+			hash = (hash << 8) | (this.bytes[this.start + hashIndex] & 0xff);
+		}
+		return hash;
+	}
+
+	@Override
+	public boolean equals(@Nullable Object obj) {
+		return this == obj || (obj instanceof ByteString && compareTo((ByteString) obj) == 0);
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder buffer = new StringBuilder();
+		int formatLimit = Math.min(16, this.length);
+
+		HexFormat.LOWER_CASE.format(buffer, this.bytes, this.start, formatLimit);
+		if (formatLimit < this.length) {
+			buffer.append(Strings.ELLIPSIS);
+		}
+		return buffer.toString();
+	}
+
+}
---
+++ src/test/java/de/carne/test/util/ByteStringTest.java
@@ -0,0 +1,155 @@
+/*
+ * Copyright (c) 2016-2020 Holger de Carne and contributors, All Rights Reserved.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package de.carne.test.util;
+
+import java.util.Arrays;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import de.carne.util.ByteString;
+
+/**
+ * Test {@linkplain ByteString} class.
+ */
+class ByteStringTest {
+
+	private static final byte[] TEST_BYTES_0 = new byte[] {};
+	private static final byte[] TEST_BYTES_1 = new byte[] { (byte) 0x00, (byte) 0x01, (byte) 0x7f, (byte) 0x80,
+			(byte) 0xfe, (byte) 0xff };
+	private static final byte[] TEST_BYTES_2 = new byte[] { (byte) 0x01, (byte) 0x01, (byte) 0x7f };
+	private static final byte[] TEST_BYTES_3 = new byte[] { (byte) 0x01, (byte) 0x01, (byte) 0x7f, (byte) 0x80,
+			(byte) 0x81, (byte) 0x82, (byte) 0x83, (byte) 0x84, (byte) 0x85, (byte) 0x86, (byte) 0x87, (byte) 0x88,
+			(byte) 0x89, (byte) 0x8a, (byte) 0x8b, (byte) 0xfe, (byte) 0xff };
+
+	@Test
+	void testWrapping() {
+		ByteString bs0 = ByteString.wrap(TEST_BYTES_0);
+
+		Assertions.assertSame(ByteString.EMPTY, bs0);
+
+		byte[] mutableBytes1 = Arrays.copyOf(TEST_BYTES_1, TEST_BYTES_1.length);
+
+		ByteString bs1 = ByteString.wrap(mutableBytes1);
+
+		Assertions.assertEquals(TEST_BYTES_1.length, bs1.length());
+
+		for (int byteIndex = 0; byteIndex < TEST_BYTES_1.length; byteIndex++) {
+			Assertions.assertEquals(TEST_BYTES_1[byteIndex], bs1.byteAt(byteIndex));
+		}
+
+		mutableBytes1[1] = (byte) 0x02;
+
+		Assertions.assertNotEquals(TEST_BYTES_1[1], bs1.byteAt(1));
+	}
+
+	@Test
+	void testCopying() {
+		ByteString bs0 = ByteString.copy(TEST_BYTES_0);
+
+		Assertions.assertSame(ByteString.EMPTY, bs0);
+
+		byte[] mutableBytes1 = Arrays.copyOf(TEST_BYTES_1, TEST_BYTES_1.length);
+
+		ByteString bs1 = ByteString.copy(mutableBytes1);
+
+		Assertions.assertEquals(TEST_BYTES_1.length, bs1.length());
+
+		for (int byteIndex = 0; byteIndex < TEST_BYTES_1.length; byteIndex++) {
+			Assertions.assertEquals(TEST_BYTES_1[byteIndex], bs1.byteAt(byteIndex));
+		}
+
+		mutableBytes1[1] = (byte) 0x02;
+
+		Assertions.assertEquals(TEST_BYTES_1[1], bs1.byteAt(1));
+	}
+
+	@Test
+	void testCopyTo() {
+		ByteString bs1 = ByteString.wrap(TEST_BYTES_1);
+		byte[] bs1Copy = new byte[TEST_BYTES_1.length];
+
+		bs1.copyTo(bs1Copy, 0);
+
+		Assertions.assertArrayEquals(TEST_BYTES_1, bs1Copy);
+	}
+
+	@Test
+	void testSlicing() {
+		ByteString bs1 = ByteString.wrap(TEST_BYTES_1);
+		ByteString bs1Begin = bs1.slice(0, 1);
+		ByteString bs1End = bs1.slice(1, TEST_BYTES_1.length - 1);
+
+		Assertions.assertEquals(1, bs1Begin.length());
+		Assertions.assertEquals(TEST_BYTES_1.length - 1, bs1End.length());
+
+		Assertions.assertEquals(TEST_BYTES_1[0], bs1Begin.byteAt(0));
+		Assertions.assertEquals(TEST_BYTES_1[1], bs1End.byteAt(0));
+	}
+
+	@Test
+	void testCompare() {
+		ByteString bs0 = ByteString.wrap(TEST_BYTES_0);
+		ByteString bs1 = ByteString.wrap(TEST_BYTES_1);
+		ByteString bs2 = ByteString.wrap(TEST_BYTES_2);
+		ByteString bs3 = ByteString.wrap(TEST_BYTES_3);
+
+		Assertions.assertEquals(0, bs0.compareTo(bs0));
+		Assertions.assertEquals(0, bs1.compareTo(bs1));
+		Assertions.assertEquals(0, bs2.compareTo(bs2));
+		Assertions.assertEquals(0, bs3.compareTo(bs3));
+
+		Assertions.assertTrue(bs0.compareTo(bs1) < 0);
+		Assertions.assertTrue(bs1.compareTo(bs2) < 0);
+		Assertions.assertTrue(bs2.compareTo(bs3) < 0);
+
+		Assertions.assertTrue(bs1.compareTo(bs0) > 0);
+		Assertions.assertTrue(bs2.compareTo(bs1) > 0);
+		Assertions.assertTrue(bs3.compareTo(bs2) > 0);
+	}
+
+	@Test
+	void testEquals() {
+		ByteString bs0 = ByteString.wrap(TEST_BYTES_0);
+		ByteString bs1 = ByteString.wrap(TEST_BYTES_1);
+		ByteString bs2 = ByteString.wrap(TEST_BYTES_2);
+		ByteString bs3 = ByteString.wrap(TEST_BYTES_3);
+		ByteString bs3a = ByteString.copy(TEST_BYTES_3);
+		ByteString bs3b = ByteString.copy(TEST_BYTES_3);
+
+		Assertions.assertEquals(bs0, bs0);
+		Assertions.assertEquals(bs1, bs1);
+		Assertions.assertEquals(bs2, bs2);
+		Assertions.assertEquals(bs3, bs3);
+		Assertions.assertEquals(bs3a, bs3b);
+		Assertions.assertEquals(bs3a.hashCode(), bs3b.hashCode());
+
+		Assertions.assertNotEquals(bs0, bs1);
+		Assertions.assertNotEquals(bs1, bs2);
+		Assertions.assertNotEquals(bs2, bs3);
+	}
+
+	@Test
+	void testToString() {
+		ByteString bs2 = ByteString.wrap(TEST_BYTES_2);
+		ByteString bs3 = ByteString.wrap(TEST_BYTES_3);
+
+		Assertions.assertEquals("01 01 7f", bs2.toString());
+		Assertions.assertEquals("01 01 7f 80 81 82 83 84 85 86 87 88 89 8a 8b fe\u2026", bs3.toString());
+	}
+
+}
